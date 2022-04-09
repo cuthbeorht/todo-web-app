f0802fff12ae66434433bc26f05bd9f096bd759f
David Sciacchettano <davidsciacchettano@gmail.com>
2022-04-09 10:39:59 -0400

    Create simple to do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todo-app';
+  todoItems: any[] = [];
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.getTodoItems().subscribe((items) => {
+      this.todoItems = items.todos;
+    });
+
+
+  }
+
+  getTodoItems(): Observable<any> {
+    console.log('Getting Todo items');
+    return this.httpClient.get('http://localhost:8000/todos');
+  }
 }
