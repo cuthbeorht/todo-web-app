e3f09e92de8230fb7ff1e26bdd2f5d428743f27a
David Sciacchettano <davidsciacchettano@gmail.com>
2022-04-11 15:06:50 -0400

    Make testing library work

--- src/app/components/title/title.component.testing-library.spec.ts
+++ src/app/components/title/title.component.testing-library.spec.ts
@@ -1,6 +1,6 @@
 import {render, screen, fireEvent} from '@testing-library/angular';
 import {TitleComponent} from "./title.component";
-import "@testing-library/jest-dom/extend-expect"
+
 
 describe('Title Componenet', ()=> {
   test('should render the supplied title', async() => {
