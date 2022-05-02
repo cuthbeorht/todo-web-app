b7c9b4bdf10ffb404171796c2718fa3a8319aebf
David <cuthbeorht@users.noreply.github.com>
2022-05-02 19:01:31 -0400

    Feat/add todo list (#5)

---
+++ src/app/components/todos/todo-list/todo-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+  todoItems: any[] = [];
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+
+    this.getTodoItems().subscribe((items) => {
+      console.log(items);
+      this.todoItems = items.todos;
+    });
+  }
+
+  getTodoItems(): Observable<any> {
+    return this.httpClient.get('http://localhost:8000/todos');
+  }
+
+}
